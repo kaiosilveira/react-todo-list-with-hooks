18e040a45797e3a6ecae65573218205d1299af63
Kaio Silveira <silveira.kaio@outlook.com>
2020-01-19 22:32:05 -0300

    list todos action implemented

--- src/App.js
+++ src/App.js
@@ -4,32 +4,38 @@ import TodoList from './components/todo-list/TodoList'
 import TodoForm from './components/todo-form/TodoForm'
 import * as API from './API'
 import './App.css'
-import { Provider } from 'react-redux'
-import store from './redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { listTodos } from './redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  const [reduxTodos, setReduxTodos] = useSelector(({ todos }) => ({ todos }))
+  console.log(reduxTodos)
+  
+  useEffect(() => {
+    dispatch(listTodos())
+  }, [])
 
-  useEffect(() => { API.listTodos().then(todos => setTodos(todos)) })
+  useEffect(() => {
+    API.listTodos().then(todos => setTodos(todos))
+  }, [])
 
   const [todos, setTodos] = useState([])
 
   const handleToggleCompleted = todo => {
-    API
-    .updateTodo({ ...todo, completed: !todo.completed })
-    .then(todo => setTodos(todos.map(t => t._id === todo._id ? todo : t)))
+    API.updateTodo({ ...todo, completed: !todo.completed }).then(todo =>
+      setTodos(todos.map(t => (t._id === todo._id ? todo : t)))
+    )
   }
 
-  const handleAddTodo = text => API
-    .addTodo({ text, completed: false })
-    .then(todo => setTodos([...todos, todo]))
+  const handleAddTodo = text =>
+    API.addTodo({ text, completed: false }).then(todo => setTodos([...todos, todo]))
 
   return (
-    <div className="App">
-      <Provider store={store}>
-      <Title text="Todo list"/>
-      <TodoList todos={todos} toggleCompleted={handleToggleCompleted}/>
-      <TodoForm onSubmit={handleAddTodo}/>
-      </Provider>
+    <div className='App'>
+      <Title text='Todo list' />
+      <TodoList todos={todos} toggleCompleted={handleToggleCompleted} />
+      <TodoForm onSubmit={handleAddTodo} />
     </div>
   )
 }
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,9 +1,42 @@
 import { createStore, applyMiddleware } from 'redux'
-import { createLogicMiddleware } from 'redux-logic'
+import { createLogicMiddleware, createLogic } from 'redux-logic'
+import * as API from '../API'
+import { createActions } from 'redux-actions'
 
-const rootReducer = (s, _) => s
-const logics = []
+const LIST_TODOS = 'LIST_TODOS'
+const LIST_TODOS_SUCCESS = 'LIST_TODOS_SUCCESS'
+const LIST_TODOS_ERROR = 'LIST_TODOS_ERROR'
 
+export const { listTodos, listTodosSuccess, listTodosError } = createActions({
+  [LIST_TODOS]: () => ({}),
+  [LIST_TODOS_SUCCESS]: todos => ({ todos }),
+  [LIST_TODOS_ERROR]: error => ({ error })
+})
+
+const listTodosLogic = createLogic({
+  type: LIST_TODOS,
+  process: (_, dispatch, done) => {
+    API.listTodos()
+      .then(data => {
+        dispatch(listTodosSuccess(data))
+      })
+      .catch(err => {
+        dispatch(listTodosError())
+      })
+      .finally(done)
+  }
+})
+
+const logics = [listTodosLogic]
+
+const rootReducer = (state = { todos: [] }, action) => {
+  switch (action.type) {
+    case LIST_TODOS_SUCCESS:
+      return { todos: action.payload.todos }
+    default:
+      return state
+  }
+}
 const store = createStore(rootReducer, applyMiddleware(createLogicMiddleware(logics)))
 
 export default store
